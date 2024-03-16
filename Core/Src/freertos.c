b597adf428abeb3fa4b4133b14d840e12c608dca
GJ <gj711911@gmail.com>
2024-03-16 16:19:59 +0800

    First Commit

---
+++ Core/Src/freertos.c
@@ -0,0 +1,238 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+
+#include "cmsis_os.h"
+#include "main.h"
+#include "task.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "infantry.h"
+#include "ins.h"
+#include "remote_keyboard.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+osThreadId defaultTaskHandle;
+osThreadId insTaskHandle;
+osThreadId gimbalTaskHandle;
+osThreadId modeTaskHandle;
+osThreadId visionTaskHandle;
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void const *argument);
+void StartInsTask(void const *argument);
+void StartGimbalTask(void const *argument);
+void StartModeTask(void const *argument);
+void StartVisionTask(void const *argument);
+
+extern void MX_USB_DEVICE_Init(void);
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize);
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize)
+{
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+    *ppxIdleTaskStackBuffer = &xIdleStack[0];
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+    /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
+/**
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
+void MX_FREERTOS_Init(void)
+{
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* USER CODE BEGIN RTOS_MUTEX */
+    /* add mutexes, ... */
+    /* USER CODE END RTOS_MUTEX */
+
+    /* USER CODE BEGIN RTOS_SEMAPHORES */
+    /* add semaphores, ... */
+    /* USER CODE END RTOS_SEMAPHORES */
+
+    /* USER CODE BEGIN RTOS_TIMERS */
+    /* start timers, add new ones, ... */
+    /* USER CODE END RTOS_TIMERS */
+
+    /* USER CODE BEGIN RTOS_QUEUES */
+    /* add queues, ... */
+    /* USER CODE END RTOS_QUEUES */
+
+    /* Create the thread(s) */
+    /* definition and creation of defaultTask */
+    osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+    defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+    /* definition and creation of insTask */
+    osThreadDef(insTask, StartInsTask, osPriorityNormal, 0, 128);
+    insTaskHandle = osThreadCreate(osThread(insTask), NULL);
+
+    /* definition and creation of gimbalTask */
+    osThreadDef(gimbalTask, StartGimbalTask, osPriorityAboveNormal, 0, 128);
+    gimbalTaskHandle = osThreadCreate(osThread(gimbalTask), NULL);
+
+    /* definition and creation of modeTask */
+    osThreadDef(modeTask, StartModeTask, osPriorityAboveNormal, 0, 128);
+    modeTaskHandle = osThreadCreate(osThread(modeTask), NULL);
+
+    /* definition and creation of visionTask */
+    osThreadDef(visionTask, StartVisionTask, osPriorityAboveNormal, 0, 256);
+    visionTaskHandle = osThreadCreate(osThread(visionTask), NULL);
+
+    /* USER CODE BEGIN RTOS_THREADS */
+    /* add threads, ... */
+    /* USER CODE END RTOS_THREADS */
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const *argument)
+{
+    /* init code for USB_DEVICE */
+    MX_USB_DEVICE_Init();
+    /* USER CODE BEGIN StartDefaultTask */
+    /* Infinite loop */
+    for (;;) {
+        osDelay(500);
+    }
+    /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_StartInsTask */
+/**
+ * @brief Function implementing the insTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartInsTask */
+void StartInsTask(void const *argument)
+{
+    /* USER CODE BEGIN StartInsTask */
+    INS_Init();
+    /* Infinite loop */
+    for (;;) {
+        INS_Task();
+        osDelay(1);
+    }
+    /* USER CODE END StartInsTask */
+}
+
+/* USER CODE BEGIN Header_StartGimbalTask */
+/**
+ * @brief Function implementing the gimbalTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartGimbalTask */
+void StartGimbalTask(void const *argument)
+{
+    /* USER CODE BEGIN StartGimbalTask */
+    GimbalInit();
+    /* Infinite loop */
+    for (;;) {
+        GimbalTask();
+        osDelay(5);
+    }
+    /* USER CODE END StartGimbalTask */
+}
+
+/* USER CODE BEGIN Header_StartModeTask */
+/**
+ * @brief Function implementing the modeTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartModeTask */
+void StartModeTask(void const *argument)
+{
+    /* USER CODE BEGIN StartModeTask */
+    /* Infinite loop */
+    for (;;) {
+        ModeTask();
+        osDelay(1);
+    }
+    /* USER CODE END StartModeTask */
+}
+
+/* USER CODE BEGIN Header_StartVisionTask */
+/**
+ * @brief Function implementing the visionTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartVisionTask */
+void StartVisionTask(void const *argument)
+{
+    /* USER CODE BEGIN StartVisionTask */
+    /* Infinite loop */
+    for (;;) {
+        VisionTask();
+        osDelay(2);
+    }
+    /* USER CODE END StartVisionTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
---
+++ user/application/src/infantry.cpp
@@ -0,0 +1,79 @@
+/**
+ *******************************************************************************
+ * @file      : infantry.cpp
+ * @brief     :
+ * @history   :
+ *  Version     Date            Author          Note
+ *  V0.9.0      yyyy-mm-dd      <author>        1. <note>
+ *******************************************************************************
+ * @attention :
+ *******************************************************************************
+ *  Copyright (c) 2024 Reborn Team, USTB.
+ *  All Rights Reserved.
+ *******************************************************************************
+ */
+/* Includes ------------------------------------------------------------------*/
+#include "infantry.h"
+
+#include "board_comm.h"
+#include "bsp_dwt.h"
+#include "chassis.h"
+#include "gimbal.h"
+#include "referee.h"
+#include "remote.h"
+#include "shoot.h"
+#include "vision.h"
+/* Private macro -------------------------------------------------------------*/
+/* Private constants ---------------------------------------------------------*/
+/* Private types -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* External variables --------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+
+/**
+ * @brief Initializes the system.
+ */
+void InfantryGimbalInit()
+{
+    remote.Init(&huart3);
+    referee.Init(&huart6);
+    gimbal.MotorInit();
+    shoot.MotorInit();
+    board_comm.Init(&hcan1, 0x111);
+}
+
+/**
+ * @brief Initializes the gimbal.
+ */
+void GimbalInit()
+{
+    gimbal.PidInit();
+    shoot.PidInit();
+    chassis.Init();
+    vision.Init();
+}
+
+/**
+ * @brief This function handles the Gimbal task.
+ */
+void GimbalTask()
+{
+    gimbal.Control();
+    shoot.Control();
+    chassis.Control();
+    board_comm.Send();
+    if (remote.GetS2() == 1 || remote.GetS2() == 3) {
+        DjiMotorSend(&hcan1, 0x1FF, (int16_t)shoot.trig_output_, (int16_t)gimbal.output_speed_[1], 0, 0);
+        DjiMotorSend(&hcan2, 0x1FF, (int16_t)-gimbal.output_speed_[0], 0, 0, 0);
+        DjiMotorSend(&hcan2, 0x200, (int16_t)shoot.fric_output_[0], (int16_t)shoot.fric_output_[1], (int16_t)shoot.trig_output_, 0);
+    } else {
+        DjiMotorSend(&hcan1, 0x1FF, 0, 0, 0, 0);
+        DjiMotorSend(&hcan2, 0x1FF, 0, 0, 0, 0);
+        DjiMotorSend(&hcan2, 0x200, 0, 0, 0, 0);
+    }
+}
+
+void VisionTask()
+{
+    vision.Ctrl();
+}
